b7d01216e5d11e622f9b0e6ecb3a86d6d9d3102f
Erik <100293508+rikjnthn@users.noreply.github.com>
2024-09-13 18:38:40 +0700

    feat: add get folders logic

--- src/components/folder-list/index.tsx
+++ src/components/folder-list/index.tsx
@@ -1,10 +1,47 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import Pocketbase, { ClientResponseError } from "pocketbase";
+
 import Folder from "../folder";
+import { FolderType, SetStateType } from "@/interface";
+import { usePocketbase } from "@/context/pocketbase-context";
+
+const getFolders = async ({
+  pb,
+  setFolders,
+}: {
+  pb: Pocketbase;
+  setFolders: SetStateType<FolderType[]>;
+}) => {
+  try {
+    const userId = pb.authStore.model?.id;
+
+    const records = await pb.collection("folders").getFullList<FolderType>({
+      filter: pb.filter("user.id = {:userId}", { userId }),
+    });
+
+    setFolders(records);
+  } catch (e) {
+    if (e instanceof ClientResponseError) {
+      console.error("Error: " + e.message);
+    }
+  }
+};
 
 const FolderList = () => {
+  const [folders, setFolders] = useState<FolderType[]>([]);
+
+  const { pb } = usePocketbase();
+
+  useEffect(() => {
+    getFolders({ pb, setFolders });
+  }, [pb]);
+
   return (
-    <div className="hide-scrollbar mt-2.5 h-full overflow-y-scroll px-1 md:mt-14">
-      <Folder name="Folder" />
+    <div className="hide-scrollbar h-full overflow-y-scroll px-1 pt-2.5 md:mt-14">
+      {folders.map((folder) => (
+        <Folder key={folder.id} name={folder.name} />
+      ))}
     </div>
   );
 };
--- src/interface/index.ts
+++ src/interface/index.ts
@@ -7,3 +7,13 @@ export interface ResetPasswordDataType {
   password: string;
   confirm_password: string;
 }
+
+export interface FolderType {
+  id: string;
+  collectionId: string;
+  collectionName: string;
+  created: string;
+  updated: string;
+  name: string;
+  users: string;
+}
