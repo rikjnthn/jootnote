433a6b653226bdd623b7b7f28d3760a1381665db
Erik <100293508+rikjnthn@users.noreply.github.com>
2024-09-15 10:50:22 +0700

    fix: redirect to login if user not authenticate

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,27 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import Pocketbase from "pocketbase";
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pb = new Pocketbase(process.env.API_URL);
+
+  const cookie = cookies().toString();
+  pb.authStore.loadFromCookie(cookie);
+
+  if (!pb.authStore.isValid) {
+    redirect("/login");
+  }
+
+  await pb
+    .collection("users")
+    .getOne(pb.authStore.model?.id)
+    .catch(() => {
+      redirect("/login");
+    });
+
+  return children;
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,7 @@ import LoginCard from "@/components/login-card";
 export default function Page() {
   const pb = new Pocketbase(process.env.API_URL);
 
-  const cookie = cookies().toString() ?? "";
+  const cookie = cookies().toString();
   pb.authStore.loadFromCookie(cookie);
 
   if (pb.authStore.isValid) {
